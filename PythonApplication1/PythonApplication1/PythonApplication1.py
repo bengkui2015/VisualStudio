dca1fc172f5b1c39a751519c35e2e6a5343cbd33
damon <bengkui2015@msn.cn>
2017-09-18 10:58:18 +0800

    FakeUser

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -64,68 +64,70 @@ with open(ln_path,'r') as f:
 print( ln1) 
 print('='* 70) #分 割线 
 print( ln2)
-
 import random 
-class FakeUser(): 
-	def fake_name( self, one_word= False, two_words= False): 
+class FakeUser: 
+	def fake_name(self, one_word= False, two_words= False): 
 		if one_word: 
-			full_name = random. choice( fn) + random. choice( ln1) 
+			full_name = random.choice(fn) + random.choice(ln1) 
 		elif two_words: 
-			full_name = random. choice( fn) + random. choice( ln2) 
-		else: full_name = random. choice( fn) + random. choice( ln1 + ln2) 
-		print( full_name) 
-	def fake_gender( self): 
-		gender = random. choice([' 男',' 女',' 未知']) 
-		print( gender)
+			full_name = random.choice(fn) + random.choice(ln2) 
+		else: 
+			full_name = random.choice(fn) + random.choice(ln1 + ln2) 
+		print(full_name) 
+	def fake_gender(self): 
+		gender = random.choice([' 男',' 女',' 未知']) 
+		print(gender)
+
 class SnsUser(FakeUser): 
-	def get_followers( self, few= True, a_lot= False): 
+	def get_followers(self,few= True,a_lot= False): 
 		if few: 
-			followers = random.randrange( 1, 50) 
+			followers = random.randrange(1,50) 
 		elif a_lot: 
-			followers = random.randrange( 200, 10000) 
+			followers = random.randrange(200,10000) 
 	print(followers) 
 user_a = FakeUser() 
 user_b = SnsUser() 
-user_a. fake_name() 
-user_b. get_followers( few= True)
+user_a.fake_name() 
+user_b.get_followers(few= True)
 
-class FakeUser(): 
-	def fake_name( self, amount= 1, one_word= False, two_words= False): 
-		n = 0 
-		while n <= amount: 
-			if one_word: 
-				full_name = random. choice( fn) + random. choice( ln1) 
-			elif two_words: 
-				full_name = random. choice( fn) + random. choice( ln2) 
-			else: 
-				full_name = random. choice( fn) + random. choice( ln1 + ln2) 
-			yield full_name
-			n+= 1 
-	def fake_gender( self, amount= 1):
-		n = 0 
-		while n <= amount: 
-			gender = random. choice([' 男',' 女',' 未知']) 
-			yield gender 
-			n += 1 
-class SnsUser( FakeUser): 
-	def get_followers( self, amount= 1, few= True, a_lot= False): 
-		n = 0 
-		while n <= amount : 
-			if few: 
-				followers = random. randrange( 1, 50) 
-			elif a_lot: 
-				followers = random. randrange( 200, 10000) 
-			yield followers 
-			n+= 1 
-user_a = FakeUser() 
-user_b = SnsUser() 
-for name in user_a. fake_names( 30): 
-	print( name) 
-	for gender in user_a. fake_gender( 30): 
-		print( gender)
 
-import sys 
-print( sys. path)
+#class FakeUser(): 
+#	def fake_name( self, amount= 1, one_word= False, two_words= False): 
+#		n = 0 
+#		while n <= amount: 
+#			if one_word: 
+#				full_name = random. choice( fn) + random. choice( ln1) 
+#			elif two_words: 
+#				full_name = random. choice( fn) + random. choice( ln2) 
+#			else: 
+#				full_name = random. choice( fn) + random. choice( ln1 + ln2) 
+#			yield full_name
+#			n+= 1 
+#	def fake_gender( self, amount= 1):
+#		n = 0 
+#		while n <= amount: 
+#			gender = random. choice([' 男',' 女',' 未知']) 
+#			yield gender 
+#			n += 1 
+#class SnsUser( FakeUser): 
+#	def get_followers( self, amount= 1, few= True, a_lot= False): 
+#		n = 0 
+#		while n <= amount : 
+#			if few: 
+#				followers = random. randrange( 1, 50) 
+#			elif a_lot: 
+#				followers = random. randrange( 200, 10000) 
+#			yield followers 
+#			n+= 1 
+#user_a = FakeUser() 
+#user_b = SnsUser() 
+#for name in user_a. fake_names( 30): 
+#	print( name) 
+#	for gender in user_a. fake_gender( 30): 
+#		print( gender)
+
+#import sys 
+#print( sys. path)
 
 
 
