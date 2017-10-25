cd7407c8b8dd651bafe94c9c09ce9c5a7779b7db
damon <damon@WANGBAORUI-BJB>
2017-10-25 17:00:21 +0800

    2017102517

--- C Primer Plus/C Primer Plus/main.c
+++ C Primer Plus/C Primer Plus/main.c
@@ -387,40 +387,95 @@
 //}
 
 /*指针操作*/
+//#include<stdio.h>
+//int main(void)
+//{
+//	int urn[5] = { 100, 200, 300, 400, 500 };
+//	int * ptr1, *ptr2, *ptr3;
+//	ptr1 = urn;
+//	ptr2 = &urn[2];
+//	printf("pointer value, dereferenced pointer, pointer adrress:\n");
+//	printf("ptr1 = %p, *ptr1 = %d, &ptr1 = %p\n", ptr1, *ptr1, &ptr1);
+//
+//	ptr3 = ptr1 + 4;
+//	printf("\nadding an int to a pointer:\n");
+//	printf("ptr1 + 4 = %p, *(ptr1 + 3) = %d\n", ptr1 + 4, *(ptr1 + 3));
+//
+//	ptr1++;		//递增指针
+//	printf("\nvalues after ptr1++\n");
+//	printf("ptr1 = %p, *ptr1 = %d, &ptr1 = %p\n", ptr1, *ptr1, &ptr1);
+//
+//	ptr2--;		//递减指针
+//	printf("\nvalues after ptr2--\n");
+//	printf("ptr2 = %p, *ptr2 = %d, &ptr2 = %p\n", ptr2, *ptr2, &ptr2);
+//
+//	--ptr1;		//恢复为初始值
+//	++ptr2;		//恢复为初始值
+//	printf("\npointers reset to original values:\n");
+//	printf("ptr1 = %p, ptr2 = %p\n", ptr1, ptr2);
+//
+////一个指针减去另一个指针
+//	printf("\nsubtracting one pointer form another:\n");
+//	printf("ptr2 = %p, ptr1 = %p, ptr2 - ptr1 = %d\n", ptr2, ptr1, ptr2 - ptr1);
+//
+//	//一个指针减去一个整数
+//	printf("\nsubtracting an int from a pointer:\n");
+//	printf("ptr3 = %p, ptr3 - 2 = %p\n", ptr3, ptr3 - 2);
+//
+//	return 0;
+//
+//}
+
+/*使用枚举值*/
 #include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+enum spectrum {red, orange, yellow, green, blue, violet};
+const char * colors[] = { "red", "orange", "yellow", "gerrn", "blue", "violet" };
+
+#define LEN 30
+
 int main(void)
 {
-	int urn[5] = { 100, 200, 300, 400, 500 };
-	int * ptr1, *ptr2, *ptr3;
-	ptr1 = urn;
-	ptr2 = &urn[2];
-	printf("pointer value, dereferenced pointer, pointer adrress:\n");
-	printf("ptr1 = %p, *ptr1 = %d, &ptr1 = %p\n", ptr1, *ptr1, &ptr1);
+	char choice[LEN];
+	enum spectrum color;
+	bool color_is_found = false;
 
-	ptr3 = ptr1 + 4;
-	printf("\nadding an int to a pointer:\n");
-	printf("ptr1 + 4 = %p, *(ptr1 + 3) = %d\n", ptr1 + 4, *(ptr1 + 3));
+	puts("Enter a color (empty line to quit):");
+	while (gets(choice) != NULL && choice[0] != '\0')
+	{
+		for (color = red; color <= violet; color++)
+			if (strcmp(choice, colors[color]) == 0)
+			{
+				color_is_found = true;
+				break;
+			}
 
-	ptr1++;		//递增指针
-	printf("\nvalues after ptr1++\n");
-	printf("ptr1 = %p, *ptr1 = %d, &ptr1 = %p\n", ptr1, *ptr1, &ptr1);
+		if (color_is_found)
+			switch (color)
+			{
+			case red:puts("Roses are red.");
+				break;
+			case orange:puts("Poppies are orange");
+				break;
+			case yellow:puts("Sunflows are yellow.");
+				break;
+			case green:puts("Grass are green");
+				break;
+			case blue:puts("Bluebells are blue.");
+				break;
+			case violet:puts("Violets are violet");
+				break;
 
-	ptr2--;		//递减指针
-	printf("\nvalues after ptr2--\n");
-	printf("ptr2 = %p, *ptr2 = %d, &ptr2 = %p\n", ptr2, *ptr2, &ptr2);
+			}
+		else
+			printf("I don't know about the color %s.\n", choice);
+		color_is_found = false;
+		puts("Next color, please(empty line to quie):");
 
-	--ptr1;		//恢复为初始值
-	++ptr2;		//恢复为初始值
-	printf("\npointers reset to original values:\n");
-	printf("ptr1 = %p, ptr2 = %p\n", ptr1, ptr2);
-
-//一个指针减去另一个指针
-	printf("\nsubtracting one pointer form another:\n");
-	printf("ptr2 = %p, ptr1 = %p, ptr2 - ptr1 = %d\n", ptr2, ptr1, ptr2 - ptr1);
-
-	//一个指针减去一个整数
-	printf("\nsubtracting an int from a pointer:\n");
-	printf("ptr3 = %p, ptr3 - 2 = %p\n", ptr3, ptr3 - 2);
+	}
+	puts("Goodbye");
 
 	return 0;
 
